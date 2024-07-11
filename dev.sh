524b8d410c241d3c95ab6fb9db33b58f967b56b4
Jefferson Carlos <jeff.carlosbd@gmail.com>
2024-07-10 22:40:39 -0300

    atualiza shell inicializador e instala dependencia

--- dev.sh
+++ dev.sh
@@ -6,10 +6,13 @@ set +o allexport
 
 docker compose --env-file .env -f docker-compose-local.yml up -d
 
+goapi-gen --package=spec --out internal/api/spec/journey.gen.spec.go internal/api/spec/journey.spec.json
+printf " \033[0;32m✔\033[0m API Specs \n"
+
 tern migrate --migrations internal/pgstore/migrations --config internal/pgstore/migrations/tern.conf
-printf " \033[0;32m✔\033[0m tern migrate \n"
+printf " \033[0;32m✔\033[0m Migrations \n"
 
 sqlc generate -f internal/pgstore/sqlc.yml
-printf " \033[0;32m✔\033[0m sqlc generate \n"
+printf " \033[0;32m✔\033[0m SQL generated \n"
 
 go run cmd/journey/journey.go
